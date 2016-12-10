51cae2b5381c4450931f2fe249ea78d114cb9a5c
Sam Borick <27thisotope@gmail.com>
2016-12-09 23:31:17 -0500

    wiki stuff

--- www/index.php
+++ www/index.php
@@ -34,8 +34,17 @@
                     <hr class="section-heading-spacer">
                     <div class="clearfix"></div>
                     <h2 class="section-heading">Automating the Election Process<br>Our Mission</h2>
-                    <p class="lead">Our goal is to bring the Order of the Arrow election process into the 21st century.
-                    With modern tools and technologies we can make the election process better for everyone
+                    <p class="lead">AutoElect is an election management engine for the Order of the Arrow.  The Order of the Arrow, or OA, is an honor society within the Boy Scouts of America.  The OA is unique in that new members are elected by non members, that is to say new OA members are picked by people who are not in the OA.  Scouts become a member of the OA via a few steps:
+Have attained a particular rank
+Have camped a certain number of nights
+Be voted worthy by a majority of scouts in their troop
+Attend a specific weekend camping event
+
+
+There are many challenges that come with co-ordinating this whole process.  One is that our Lodge, or local branch of the OA, covers over 2,000 square miles and includes over 1,000 members.  In order to coordinate the elections requires a lot of effort on the person coordinating them.  Another challenge is that often steps 3 and 4 can take place many months apart, and sometimes new member data gets lost in the process.  AutoElect solves these problems.
+
+
+AutoElect serves as a sort of ‘automatic administrator’, reminding the real people running the organization to do things at preset times, such as inputting data.  This is accomplished via a series of dynamic PHP forms and a robust backend that can schedule tasks at any time in the future, even years ahead.  Attached is a flowchart that shows the general workflow for AutoElect over a single election cycle.
                     </p>
                 </div>
             </div>
