34cd25a776b40d89af46daa4cd076a295c713f99
mibzmam <27thisotope@gmail.com>
2017-01-08 15:15:07 -0500

    fixed navbar overlap and old href links

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
-  templateUrl: 'app/app.component.html'
+  templateUrl: 'app/app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   pageTitle = 'AutoElect';
