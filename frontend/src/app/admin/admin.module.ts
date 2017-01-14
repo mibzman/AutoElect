febbad0069891b236f11892e60a41afd6a14181f
mibzmam <27thisotope@gmail.com>
2017-01-14 16:36:46 -0500

    added encryption to login

--- frontend/src/app/admin/admin.module.ts
+++ frontend/src/app/admin/admin.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule }   from '@angular/forms';
 
 import { UIModule } from '../ui/ui.module';
 
@@ -8,16 +9,19 @@ import { DashComponent } from './dash/dash.component';
 import { AdminHeaderComponent } from '../ui/adminheader/adminheader.component';
 
 import { LoginGuard } from './login-guard.service';
+import { TroopsComponent } from './troops/troops.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forRoot([
       // { path: '', canActivate: [LoginGuard],  component: DashComponent },
       // { path: 'dash/*', component: AdminHeaderComponent, outlet: 'header'}
       { path: 'admin/:lodgeName',  canActivate: [LoginGuard], component: AdminHeaderComponent,
         children: [
           { path: 'dash', component: DashComponent},
+          { path: 'troops', component: TroopsComponent},
           { path: '', redirectTo: 'dash', pathMatch: 'full'},
           // { path: '', component: AdminHeaderComponent, outlet: 'header'},
           // { path: '', component: AdminFooterComponent, outlet: 'footer'}
@@ -33,6 +37,7 @@ import { LoginGuard } from './login-guard.service';
   ],
   declarations: [
     DashComponent,
+    TroopsComponent,
   ]
 })
 export class AdminModule { }
--- frontend/src/app/entry/login/login.service.ts
+++ frontend/src/app/entry/login/login.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
+import {Md5} from 'ts-md5/dist/md5';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/observable/throw';
 
 import {ILogin} from './ILogin';
-//fuck
 
 @Injectable()
 export class LoginService {
@@ -17,27 +17,11 @@ export class LoginService {
   constructor(private _http: Http) {}
 
   canLogin(username:string, password:string): Observable<ILogin> {
-    // return this._http.get(this._productUrl)
-    //     .map((response: Response) => <ILogin> response.json())
-    //     .do(data => console.log('All: ' +  JSON.stringify(data)))
-    //     .catch(this.handleError);
-    //this will be reimplemented once there's an actuial api
-    // if (username == "admin"){
-    //
-    // }else{
-    //   return this._http.get('api/nologin.json')
-    //       .map((response: Response) => <ILogin> response.json())
-    //       .do(data => console.log('All: ' +  JSON.stringify(data)))
-    //       .catch(this.handleError);
-    // }
-
-    var url = this._productUrl + username + "?hash=" + password;
+    var url = this._productUrl + username + "?hash=" + Md5.hashStr(password);
     console.log(url);
     return this._http.get(url)
         .map((response: Response) => <ILogin> response.json())
-        //.do(data => console.log('All: ' +  JSON.stringify(data)))
-        //.catch(this.handleError);
-
+        .catch(this.handleError);
   }
 
   private handleError(error: Response) {
